f928c3c3d00da9963aad459108162279183a24dd
shirow ozawa <striderkein@users.noreply.github.com>
2021-07-31 21:30:07 +0900

    refactor:placeholderとlabelを変更

--- app/assigns/components/AssignForm.tsx
+++ app/assigns/components/AssignForm.tsx
@@ -8,13 +8,13 @@ export { FORM_ERROR } from "app/core/components/Form"
 export function AssignForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
     <Form<S> {...props}>
-      <LabeledTextField name="start" label="期間（開始）" placeholder="期間（開始）" />
-      <LabeledTextField name="end" label="期間（終了）" placeholder="期間（終了）" />
-      <LabeledTextField name="utilization" label="稼働率" placeholder="稼働率" type="number" />
+      <LabeledTextField name="start" label="期間（開始）" placeholder="YYYY-MM-DD" />
+      <LabeledTextField name="end" label="期間（終了）" placeholder="YYYY-MM-DD" />
+      <LabeledTextField name="utilization" label="稼働率（%）" placeholder="50" type="number" />
       <LabeledTextField
         name="staffs.0.name"
         label="アサインスタッフ"
-        placeholder="アサインスタッフ"
+        placeholder="山田太郎"
         type="number"
       />
       <ul>
